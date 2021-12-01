9ff47888c92098cf9c03dc18e20f7a152b996299
Lauren <you@example.com>
2021-12-01 17:15:58 -0600

    Adding in Forms

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from MainApp.forms import EntryForm, TopicForm
 from .models import Topic
 # Create your views here.
 def index(request):
@@ -16,5 +18,34 @@ def topic(request, topic_id):
     entries = topic.entry_set.all()
     context = {"topic":topic, 'entries':entries}
     return render(request, "MainApp/topic.html", context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('MainApp:topics')
+    #context is a dictionary that helps us pass data
+    context = {'form':form}
+    return render(request, 'MainApp/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form =EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+
+            return redirect('MainApp:topic', topic_id=topic_id )
+    context = {'form':form}
+    return render(request, 'MainApp/new_entry.html', context)
+
+
 
 
