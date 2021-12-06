4b0f9b31a6a80dee6d01bf4b2f3772960bcdc1ca
Lauren <you@example.com>
2021-12-06 17:27:42 -0600

    Adding login to pages

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,23 +2,30 @@ from django.shortcuts import render, redirect
 
 from MainApp.forms import EntryForm, TopicForm
 from .models import Topic, Entry
+
+from django.contrib.auth.decorators import login_required
+
+from django.http import Http404
+
 # Create your views here.
 def index(request):
     return render(request, 'MainApp/index.html')
 
-
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     #key is used in html, value is used in the view function
     context = {'topics':topics}
     return render(request, 'MainApp/topics.html', context)
-
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id = topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.all()
     context = {"topic":topic, 'entries':entries}
     return render(request, "MainApp/topic.html", context)
-
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
@@ -30,7 +37,7 @@ def new_topic(request):
     #context is a dictionary that helps us pass data
     context = {'form':form}
     return render(request, 'MainApp/new_topic.html', context)
-
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
@@ -45,7 +52,7 @@ def new_entry(request, topic_id):
             return redirect('MainApp:topic', topic_id=topic_id )
     context = {'form':form, 'topic':topic}
     return render(request, 'MainApp/new_entry.html', context)
-
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
@@ -56,7 +63,7 @@ def edit_entry(request, entry_id):
         form = EntryForm(instance=entry, data = request.POST)
         if form.is_valid():
             form.save()
-            return redirect('MainApp:topic', topic_id = topic_id)
+            return redirect('MainApp:entry', topic_id = topic.id)
     
     context = {'entry':entry, 'topic':topic, 'form':form}
     return render(request, 'MainApp/edit_entry.html', context)
